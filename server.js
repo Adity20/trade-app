7831cfb6f9e5878b3b5e46a05bb11bb50b714459
Aditya Sharma <sharma.aditya9164@gmail.com>
2024-06-25 15:36:18 +0530

    connecting with the database using mongodb atlas

--- server.js
+++ server.js
@@ -4,3 +4,16 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const routes = require('./routes/tradeRoute');
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+});
+
+app.use(bodyParser.json());
+app.use('/', routes);
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Server is running on port', process.env.PORT || 3000);
+});
